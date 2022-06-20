fae65ede5f87f016406cabcbff00ff2305162058
fanjatiana.pavageau <fanjatiana.pavageau@outlook.com>
2022-06-20 22:45:22 +0200

    feat: installing react router and project folders / documents

---
+++ src/router/Router.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import { Route, Routes } from 'react-router-dom';
+
+const Router = () => {
+    return (
+        <>
+            <Routes>
+                <Route  path="/A-propos" element="" />
+                <Route  path="/Mes-projets" element="" />
+                <Route  path="/Contactez-moi" element="" />
+            </Routes>
+        </>
+    );
+};
+
+export default Router;
